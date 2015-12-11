77c60c5957ee1119d699ed319cbceee0eafe5330
Denny Britz <dennybritz@gmail.com>
2015-12-11 08:14:25 +0100

    Add L2 regularization loss

--- train.py
+++ train.py
@@ -16,6 +16,8 @@ tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embed
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
+tf.flags.DEFINE_float("l2_reg_lambda", 0.1, "L2 regularizaion lambda (default: 0.1)")
+
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
 tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (default: 100)")
@@ -67,7 +69,8 @@ with tf.Graph().as_default():
             vocab_size=len(vocabulary),
             embedding_size=FLAGS.embedding_dim,
             filter_sizes=map(int, FLAGS.filter_sizes.split(",")),
-            num_filters=FLAGS.num_filters)
+            num_filters=FLAGS.num_filters,
+            l2_reg_lambda=FALGS.l2_reg_lambda)
 
         # Define Training procedure
         global_step = tf.Variable(0, name="global_step", trainable=False)

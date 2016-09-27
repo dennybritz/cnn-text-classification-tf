326db295cb2224830b938c183d56391f5b406f0d
LargonD <largon89@gmail.com>
2016-09-27 14:48:30 +0200

    Update train.py
    Multiclass generalization

--- train.py
+++ train.py
@@ -73,7 +73,7 @@ with tf.Graph().as_default():
     with sess.as_default():
         cnn = TextCNN(
             sequence_length=x_train.shape[1],
-            num_classes=2,
+            num_classes=y_train.shape[1],
             vocab_size=len(vocab_processor.vocabulary_),
             embedding_size=FLAGS.embedding_dim,
             filter_sizes=list(map(int, FLAGS.filter_sizes.split(","))),

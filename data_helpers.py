7c704491a41d85290c350f392f9253568190d8d9
bly <bly930725@gmail.com>
2016-12-18 20:26:05 -0700

    fix bug for batch_iter

--- data_helpers.py
+++ data_helpers.py
@@ -51,7 +51,7 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
     """
     data = np.array(data)
     data_size = len(data)
-    num_batches_per_epoch = int(len(data)/batch_size) + 1
+    num_batches_per_epoch = int((len(data)-1)/batch_size) + 1
     for epoch in range(num_epochs):
         # Shuffle the data at each epoch
         if shuffle:
